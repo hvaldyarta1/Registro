e0db24196aa1735395b93819987cd1ee01376f83
hvaldyarta1 <v.aldyarta@gmail.com>
2018-12-13 08:22:48 +0700

    adapter button

---
+++ app/src/main/java/app/rsprmobile/registro/adapter/AdapterDataKlinik.java
@@ -0,0 +1,94 @@
+package app.rsprmobile.registro.adapter;
+
+import android.content.Context;
+import android.content.DialogInterface;
+import android.support.annotation.NonNull;
+import android.support.v7.app.AlertDialog;
+import android.support.v7.widget.RecyclerView;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.Toast;
+
+import java.sql.Array;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Objects;
+
+import app.rsprmobile.registro.Pendaftaran;
+import app.rsprmobile.registro.R;
+
+
+public class AdapterDataKlinik extends RecyclerView.Adapter<AdapterDataKlinik.ViewHolder> {
+    private Context context;
+    private ArrayList<Integer> arrayJumlah;
+    // private List<DataKlinik> itemDataKlinik;
+
+    public AdapterDataKlinik(Context context, ArrayList<Integer> arrayJumlah){
+        this.context = context;
+        this.arrayJumlah = arrayJumlah;
+    }
+
+
+    @NonNull
+    @Override
+    public AdapterDataKlinik.ViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+        View v = LayoutInflater.from(parent.getContext()).inflate(R.layout.button_nomor, null);
+
+        return new ViewHolder(v);
+    }
+
+    @Override
+    public void onBindViewHolder(@NonNull final AdapterDataKlinik.ViewHolder viewHolder, final int position) {
+        viewHolder.button.setText(String.valueOf(arrayJumlah.get(position).toString()));
+
+        /*viewHolder.button.setBackgroundColor(viewHolder.button.getContext().getResources().getColor(R.color.colorPrimaryDark));*/
+
+        viewHolder.button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                String txt = viewHolder.button.getText().toString();
+                Toast.makeText(v.getContext(), txt, Toast.LENGTH_SHORT).show();
+
+                final Pendaftaran pendaftaran = new Pendaftaran();
+                pendaftaran.noAntrian = txt;
+
+                AlertDialog.Builder builder = new AlertDialog.Builder(context);
+                builder.setTitle("Konfirmasi pendaftaran periksa?");
+                builder.setPositiveButton(android.R.string.yes, new DialogInterface.OnClickListener() {
+                    public void onClick(DialogInterface dialog, int id) {
+                        //TODO
+                        pendaftaran.daftarPeriksaKunjungan();
+                    }
+                });
+                builder.setNegativeButton(android.R.string.cancel, new DialogInterface.OnClickListener() {
+                    public void onClick(DialogInterface dialog, int id) {
+                        //TODO
+                        dialog.dismiss();
+                    }
+                });
+                AlertDialog dialog = builder.create();
+                dialog.show();
+
+            }
+        });
+
+    }
+
+
+    @Override
+    public int getItemCount() {
+        return arrayJumlah.size();
+    }
+
+    public class ViewHolder extends RecyclerView.ViewHolder {
+        Button button;
+
+        public ViewHolder(@NonNull View itemView) {
+            super(itemView);
+
+            button = (Button) itemView.findViewById(R.id.btnNomor);
+        }
+    }
+}
